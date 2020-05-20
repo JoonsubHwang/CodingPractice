a907b65d208ffdcdf83704c65505fa02fb82e5dd
Joonsub Hwang <56992290+JoonsubHwang@users.noreply.github.com>
2020-05-19 23:27:42 -0400

    Create SortOddNums.cpp

--- ShortProblems/CodeWars/SortOddNums.cpp
+++ ShortProblems/CodeWars/SortOddNums.cpp
@@ -1,1 +1,27 @@
+/*
+    algorithm
+    https://www.codewars.com/kata/578aa45ee9fd15ff4600090d/cpp
+    
+    
+    only sort odd numbers in an array
+*/
 
+#include <utility>
+using namespace std;
+
+vector<int> sortArray(vector<int> nums)
+{
+  vector<int> odds;
+
+  for (int n : nums) // take out odd numbers
+    if (n % 2 == 1)
+      odds.push_back(n);
+
+  sort(odds.begin(), odds.end()); // sort odd numbers
+
+    for (int iOdds = 0, iNums = 0; iNums < nums.size(); ++iNums) // put odds numbers back in
+      if (nums[iNums] % 2 == 1)
+        nums[iNums] = odds[iOdds++];
+
+  return nums;
+}
